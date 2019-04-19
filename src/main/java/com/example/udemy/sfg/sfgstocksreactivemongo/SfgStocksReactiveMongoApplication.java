5391cbdd2d807d983a622b5e481d1bc37700ed15
SergeA <ronnieo@gmail.com>
2019-04-19 14:16:50 -0400

    Initial Commit

---
+++ src/main/java/com/example/udemy/sfg/sfgstocksreactivemongo/SfgStocksReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy.sfg.sfgstocksreactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgStocksReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgStocksReactiveMongoApplication.class, args);
+	}
+
+}
